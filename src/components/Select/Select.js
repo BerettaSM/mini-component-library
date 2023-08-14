baac813c0c7ca67b63af02326cb790bc2d0df531
Ramon Saviato <ramonsaviato@hotmail.com>
2023-08-14 11:14:41 -0300

    Update Select.js

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,71 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from 'react'
+import styled from 'styled-components'
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from '../../constants'
+import Icon from '../Icon'
+import { getDisplayedValue } from './Select.helpers'
 
 const Select = ({ label, value, onChange, children }) => {
-  const displayedValue = getDisplayedValue(value, children);
+    const displayedValue = getDisplayedValue(value, children)
 
-  return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
-  );
-};
+    return (
+        <Wrapper>
+            <NativeSelect value={value} onChange={onChange}>
+                {children}
+            </NativeSelect>
+            <PresentationalBit>
+                {displayedValue}
+                <IconWrapper style={{ '--size': 24 + 'px' }}>
+                    <Icon id="chevron-down" strokeWidth={1} size={24} />
+                </IconWrapper>
+            </PresentationalBit>
+        </Wrapper>
+    )
+}
 
-export default Select;
+const Wrapper = styled.div`
+    position: relative;
+    width: max-content;
+`
+
+const NativeSelect = styled.select`
+    position: absolute;
+    inset: 0;
+    height: 100%;
+    width: 100%;
+    opacity: 0;
+    appearance: none;
+`
+
+const PresentationalBit = styled.div`
+    color: ${COLORS.gray700};
+    background-color: ${COLORS.transparentGray15};
+    font-size: 1rem;
+    padding: 12px 16px;
+    padding-right: 52px;
+    border-radius: 8px;
+
+    transition: color 200ms ease-in;
+
+    ${NativeSelect}:focus + & {
+        outline: 1px dotted #212121;
+        outline: 5px auto -webkit-focus-ring-color;
+    }
+
+    ${NativeSelect}:hover + & {
+        color: black;
+    }
+`
+
+const IconWrapper = styled.div`
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    right: 10px;
+    margin: auto;
+    width: var(--size);
+    height: var(--size);
+    pointer-events: none;
+`
+
+export default Select
